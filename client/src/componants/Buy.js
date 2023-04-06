f1fac8d73154a57133b8c48b5b8fe100ba54254b
Jasmeen Maradeeya <jasmeen.maradeeya@gmail.com>
2023-04-06 10:14:37 +0530

    msg added

--- client/src/componants/Buy.js
+++ client/src/componants/Buy.js
@@ -20,6 +20,7 @@ const Buy = ({ state, setRerender, rerender }) => {
 			<>
 				<form onSubmit={buyChai} className="container-md card w-50 p-3 mt-5">
 					<div className="fs-2 text-center">Buy Chai</div>
+					<p className="text-center">Please connect to Goerli Testnet to load data</p>
 					<div className="mb-3">
 						<label htmlFor="name" className="form-label">Name</label>
 						<input type="text" id="name" onChange={(e) => setName(e.target.value)} placeholder="Enter your name" className="form-control"/>
@@ -28,7 +29,7 @@ const Buy = ({ state, setRerender, rerender }) => {
 						<label htmlFor="message" className="form-label"	>Message</label>
 						<input type="text" id="message" onChange={(e) => setMessage(e.target.value)} placeholder="Enter your Message" className="form-control"/>
 					</div>
-					<button type="submit" className="btn btn-primary" disabled={!account}>Pay</button>
+					<button type="submit" className="btn btn-primary" disabled={!account}>Pay 0.001Eth</button>
 				</form>
 			</>
     )
